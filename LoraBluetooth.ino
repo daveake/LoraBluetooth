d71a7af2aca2d65aa108447ffbcb0fc1ed6f76b9
Kevin Walton <kevin@unseen.org>
2020-05-14 11:13:10 +0100

    Changed #define to OLED_RST not OLED_RESET
    Changed #define to OLED_RST not OLED_RESET as the rest of the code uses OLED_RST

--- LoraBluetooth.ino
+++ LoraBluetooth.ino
@@ -38,7 +38,7 @@
   #define ESP32
   #define BLUE
   #define OLED
-  #define OLED_RESET         16
+  #define OLED_RST         16
   #define OLED_SDA            4
   #define OLED_SCL           15
   #define SCREEN_WIDTH      128
@@ -55,7 +55,7 @@
   #define ESP32
   #define BLUE
   #define OLED
-  #define OLED_RESET         16
+  #define OLED_RST         16
   #define OLED_SDA           21
   #define OLED_SCL           22
   #define SCREEN_WIDTH      128
